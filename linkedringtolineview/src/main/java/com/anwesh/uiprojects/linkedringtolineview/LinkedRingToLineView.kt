6937b835da4d6312d179e1a1324819eca0d414e9
Anwesh43 <anweshthecool0@gmail.com>
2018-08-13 01:44:37 +0530

    importing libraries and defining nodes

--- linkedringtolineview/src/main/java/com/anwesh/uiprojects/linkedringtolineview/LinkedRingToLineView.kt
+++ linkedringtolineview/src/main/java/com/anwesh/uiprojects/linkedringtolineview/LinkedRingToLineView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.linkedringtolineview
 /**
  * Created by anweshmishra on 13/08/18.
  */
+
+import android.view.View
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Path
+
+val nodes : Int = 5
