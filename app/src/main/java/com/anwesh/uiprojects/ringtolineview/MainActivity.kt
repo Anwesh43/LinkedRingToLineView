38954101c7c73e056e8994b2dd5c37aa378875cb
Anwesh43 <anweshthecool0@gmail.com>
2018-08-13 02:37:03 +0530

    adding View in activity

--- app/src/main/java/com/anwesh/uiprojects/ringtolineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/ringtolineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.ringtolineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedringtolineview.LinkedRingToLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedRingToLineView.create(this)
     }
 }
