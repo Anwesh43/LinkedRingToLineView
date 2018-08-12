4fbda722ecd78292aaaec62b02eb52e0e67810c4
Anwesh43 <anweshthecool0@gmail.com>
2018-08-13 01:48:17 +0530

    adding LinkedRingToLineView class

--- linkedringtolineview/src/main/java/com/anwesh/uiprojects/linkedringtolineview/LinkedRingToLineView.kt
+++ linkedringtolineview/src/main/java/com/anwesh/uiprojects/linkedringtolineview/LinkedRingToLineView.kt
@@ -9,5 +9,24 @@ import android.content.Context
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Path
+import android.view.MotionEvent
 
 val nodes : Int = 5
+
+class LinkedRingToLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
