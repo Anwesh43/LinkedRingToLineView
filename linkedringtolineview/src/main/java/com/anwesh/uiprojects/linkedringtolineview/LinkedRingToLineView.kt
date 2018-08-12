9f0d4ebc05d696a91d22632539f4bcbe135c594d
Anwesh43 <anweshthecool0@gmail.com>
2018-08-13 02:07:01 +0530

    adding LRLNode class

--- linkedringtolineview/src/main/java/com/anwesh/uiprojects/linkedringtolineview/LinkedRingToLineView.kt
+++ linkedringtolineview/src/main/java/com/anwesh/uiprojects/linkedringtolineview/LinkedRingToLineView.kt
@@ -102,4 +102,49 @@ class LinkedRingToLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRLNode(var i : Int, val state : State = State()) {
+
+        private var next : LRLNode? = null
+
+        private var prev : LRLNode? = null
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LRLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        init {
+            addNeighbor()
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRLNode {
+            var curr : LRLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
