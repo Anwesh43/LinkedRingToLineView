41ddb4dff39e07737a2f07fe4259846e4ca2b101
Anwesh43 <anweshthecool0@gmail.com>
2018-08-13 10:59:33 +0530

    adding AnimatiAnimationListener in activity

--- app/src/main/java/com/anwesh/uiprojects/ringtolineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/ringtolineview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.ringtolineview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.linkedringtolineview.LinkedRingToLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedRingToLineView.create(this)
+        val view : LinkedRingToLineView = LinkedRingToLineView.create(this)
         fullScreen()
+        view.addAnimationListener({createToast("animation ${it + 1} is complete")}, {createToast("animation ${it + 1} is reset")})
+    }
+
+    fun createToast(msg : String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
 
